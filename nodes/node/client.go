2911c7718c7b26c9862ce26a5d06d70b0067b2a2
Aaron Chang <xesuschang@gmail.com>
2019-07-23 10:18:03 +0800

    2: refactor, reduce arguments count

--- nodes/node/client.go
+++ nodes/node/client.go
@@ -1,6 +1,12 @@
 package node
 
 import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+
+	"github.com/bitmark-inc/bitmarkd/blockdigest"
+	"github.com/bitmark-inc/bitmarkd/fault"
 	"github.com/bitmark-inc/bitmarkd/util"
 	"github.com/bitmark-inc/bitmarkd/zmqutil"
 	"github.com/jamieabc/bitmarkd-broadcast-monitor/configuration"
@@ -11,6 +17,8 @@ type NodeClient interface {
 	BroadcastReceiver() *zmqutil.Client
 	Close()
 	CommandSenderAndReceiver() *zmqutil.Client
+	DigestOfHeight(height uint64) (*blockdigest.Digest, error)
+	Info() (*clientStatus, error)
 }
 
 type NodeClientImpl struct {
@@ -18,13 +26,34 @@ type NodeClientImpl struct {
 	commandSenderAndReciever *zmqutil.Client
 }
 
+type connectionInfo struct {
+	addressAndPort string
+	chain          string
+	zmqType        zmq.Type
+}
+
+type clientStatus struct {
+	Version string `json:"version"`
+	Chain   string `json:"chain"`
+	Normal  bool   `json:"normal"`
+	Height  uint64 `json:"height"`
+}
+
 func newClient(config configuration.NodeConfig, nodeKey *nodeKeys) (NodeClient, error) {
-	broadcastReceiver, err := newZmqClient(broadcastAddressAndPort(config), nodeKey, config.Chain)
+	broadcastReceiver, err := newZmqClient(nodeKey, connectionInfo{
+		addressAndPort: broadcastAddressAndPort(config),
+		chain:          config.Chain,
+		zmqType:        zmq.SUB,
+	})
 	if nil != err {
 		return nil, err
 	}
 
-	commandSenderAndReceiver, err := newZmqClient(commandAddressAndPort(config), nodeKey, config.Chain)
+	commandSenderAndReceiver, err := newZmqClient(nodeKey, connectionInfo{
+		addressAndPort: commandAddressAndPort(config),
+		chain:          config.Chain,
+		zmqType:        zmq.REQ,
+	})
 	if nil != err {
 		return nil, err
 	}
@@ -35,13 +64,13 @@ func newClient(config configuration.NodeConfig, nodeKey *nodeKeys) (NodeClient, 
 	}, nil
 }
 
-func newZmqClient(addressAndPort string, nodeKey *nodeKeys, chain string) (client *zmqutil.Client, err error) {
-	address, err := util.NewConnection(addressAndPort)
+func newZmqClient(nodeKey *nodeKeys, info connectionInfo) (client *zmqutil.Client, err error) {
+	address, err := util.NewConnection(info.addressAndPort)
 	if nil != err {
 		return nil, err
 	}
 
-	client, err = zmqutil.NewClient(zmq.SUB, nodeKey.private, nodeKey.public, 0)
+	client, err = zmqutil.NewClient(info.zmqType, nodeKey.private, nodeKey.public, 0)
 	if nil != err {
 		return nil, err
 	}
@@ -51,7 +80,7 @@ func newZmqClient(addressAndPort string, nodeKey *nodeKeys, chain string) (clien
 		}
 	}()
 
-	err = client.Connect(address, nodeKey.remotePublic, chain)
+	err = client.Connect(address, nodeKey.remotePublic, info.chain)
 	if nil != err {
 		return nil, err
 	}
@@ -84,4 +113,54 @@ func (n *NodeClientImpl) closeCommandSenderAndReceiver() {
 // CommandSenderAndReceiver - zmq client of command sender and receiver
 func (c *NodeClientImpl) CommandSenderAndReceiver() *zmqutil.Client {
 	return c.commandSenderAndReciever
+}
+
+// DigestOfHeight - digest of block height
+func (n *NodeClientImpl) DigestOfHeight(height uint64) (*blockdigest.Digest, error) {
+	client := n.commandSenderAndReciever
+	params := make([]byte, 8)
+	binary.BigEndian.PutUint64(params, height)
+	err := client.Send("H", params)
+	if nil != err {
+		return nil, err
+	}
+
+	data, err := client.Receive(0)
+	if nil != err {
+		return nil, err
+	}
+	if string(data[0]) != "H" || 2 != len(data) {
+		return nil, fault.ErrInvalidPeerResponse
+	}
+
+	d := blockdigest.Digest{}
+	err = blockdigest.DigestFromBytes(&d, data[1])
+	return &d, err
+}
+
+// Info - remote client info
+func (n *NodeClientImpl) Info() (*clientStatus, error) {
+	client := n.commandSenderAndReciever
+	err := client.Send("I")
+	if nil != err {
+		return nil, err
+	}
+
+	data, err := client.Receive(0)
+	if nil != err {
+		return nil, err
+	}
+
+	if "I" != string(data[0]) {
+		return nil, fmt.Errorf("wrong command")
+	}
+
+	var info clientStatus
+	err = json.Unmarshal(data[1], &info)
+	if nil != err {
+		return nil, err
+	}
+	fmt.Printf("info: %+v\n", info)
+
+	return &info, nil
 }
