6fb0ba629acaf6a8196bd2cb8e5bb8db1694d13e
Aaron Chang <xesuschang@gmail.com>
2019-09-11 17:43:13 +0800

    add block monitor

--- recorder/blocks.go
+++ recorder/blocks.go
@@ -20,24 +20,118 @@ type block struct {
 
 func (b block) isEmpty() bool {
 	return uint64(0) == b.number || "" == b.hash
+
+}
+
+// fork - fork data structure
+type fork struct {
+	Begin     uint64
+	End       uint64
+	ExpiredAt time.Time
 }
 
 type blocks struct {
 	sync.Mutex
-	data [dataLength]block
-	id   int
+	data           [dataLength]block
+	latestBlock    block
+	forkInProgress bool
+	forks          []fork
+	id             int
 }
 
 // Add - add block item
 func (b *blocks) Add(t time.Time, args ...interface{}) {
-	number := args[0].(uint64)
-	hash := args[1].(string)
-	b.data[b.id] = block{
+	nextBlock := newBlock(t, args)
+	processFork(b, nextBlock)
+	b.addBlock(nextBlock)
+	b.updateLatestBlock(nextBlock)
+}
+
+func newBlock(t time.Time, args []interface{}) block {
+	number, hash := parseBlockArguments(args)
+	return block{
 		hash:         hash,
 		number:       number,
 		receivedTime: t,
 	}
+}
+
+func parseBlockArguments(args []interface{}) (uint64, string) {
+	number := args[0].(uint64)
+	hash := args[1].(string)
+	return number, hash
+}
+
+func processFork(b *blocks, nextBlock block) {
+	if isFork(b, nextBlock) {
+		updateForkInfo(b, nextBlock)
+	} else {
+		resetForkFlag(b)
+	}
+}
+
+// each block should only be broadcast once
+func isFork(b *blocks, nextBlock block) bool {
+	if b.latestBlock.isEmpty() {
+		return false
+	}
+
+	if b.latestBlock.number > nextBlock.number {
+		return true
+	}
+
+	if b.latestBlock.number == nextBlock.number && b.latestBlock.hash != nextBlock.hash {
+		return true
+	}
+
+	return false
+}
+
+func updateForkInfo(b *blocks, nextBlock block) {
+	if isForkNotStart(b) {
+		processForkStart(b, nextBlock)
+	} else {
+		if isForkLastBlock(b, nextBlock) {
+			b.forkInProgress = false
+		}
+	}
+}
+
+func isForkNotStart(b *blocks) bool {
+	return !b.forkInProgress
+}
+
+func processForkStart(b *blocks, nextBlock block) {
+	if b.latestBlock.number != nextBlock.number {
+		b.forkInProgress = true
+	}
+	b.forks = append(b.forks, fork{
+		Begin:     nextBlock.number,
+		End:       b.latestBlock.number,
+		ExpiredAt: b.latestBlock.receivedTime.Add(expiredTimeInterval),
+	})
+}
+
+func isForkLastBlock(b *blocks, nextBlock block) bool {
+	return b.latestBlock.number == nextBlock.number
+}
+
+// in case block broadcast is dropped, make sure when higher block comes, reset flag
+func resetForkFlag(b *blocks) {
+	if b.forkInProgress {
+		b.forkInProgress = false
+	}
+}
+
+func (b *blocks) addBlock(nextBlock block) {
+	b.data[b.id] = nextBlock
 	nextID(b)
+}
+
+func (b *blocks) updateLatestBlock(nextBlock block) {
+	if nextBlock.number >= b.latestBlock.number && nextBlock.hash != b.latestBlock.hash {
+		b.latestBlock = nextBlock
+	}
 }
 
 func nextID(b *blocks) {
@@ -57,14 +151,16 @@ loop:
 			break loop
 
 		case <-timer.C:
-			cleanupExpiredBlocks(b)
+			now := time.Now()
+			cleanupExpiredBlocks(b, now)
+			cleanupExpiredForks(b, now)
 			timer.Reset(expiredTimeInterval)
 		}
 	}
 }
 
-func cleanupExpiredBlocks(b *blocks) {
-	expiredTime := time.Now().Add(-1 * expiredTimeInterval)
+func cleanupExpiredBlocks(b *blocks, now time.Time) {
+	expiredTime := now.Add(-1 * expiredTimeInterval)
 	startIdx, endIdx := indexNotFound, indexNotFound
 	for i := 0; i < dataLength; i++ {
 		if !b.data[i].isEmpty() && b.data[i].receivedTime.After(expiredTime) {
@@ -87,10 +183,26 @@ func cleanupExpiredBlocks(b *blocks) {
 	b.id = endIdx - startIdx + 1
 }
 
+func cleanupExpiredForks(b *blocks, now time.Time) {
+	endIdx := indexNotFound
+	for i, f := range b.forks {
+		if now.After(f.ExpiredAt) {
+			endIdx = i
+		}
+	}
+
+	if endIdx >= len(b.forks) {
+		b.forks = make([]fork, 0)
+	} else {
+		b.forks = b.forks[endIdx+1:]
+	}
+}
+
 // Summary - summarize blocks stat
 func (b *blocks) Summary() interface{} {
 	return BlocksSummary{
 		Duration: findDuration(b),
+		Forks:    b.forks,
 	}
 }
 
@@ -113,16 +225,10 @@ func findDuration(b *blocks) time.Duration {
 	return time.Now().Sub(earliestTime)
 }
 
-// Fork - fork data structure
-type Fork struct {
-	Begin uint64
-	End   uint64
-}
-
 // BlocksSummary - block summary data structure
 type BlocksSummary struct {
 	Duration time.Duration
-	Forks    []Fork
+	Forks    []fork
 }
 
 func (b *BlocksSummary) String() string {
@@ -131,5 +237,7 @@ func (b *BlocksSummary) String() string {
 
 // NewBlocks - new blocks data structure
 func NewBlocks() Recorder {
-	return &blocks{}
+	return &blocks{
+		forks: make([]fork, 0),
+	}
 }
--- recorder/blocks_test.go
+++ recorder/blocks_test.go
@@ -115,3 +115,147 @@ func TestBlocksRemoveOutdatedPeriodicallyWhenManyExpiration(t *testing.T) {
 	assert.True(t, s.Duration <= 2*time.Hour, "wrong larger duration")
 	assert.True(t, s.Duration >= 119*time.Minute, "wrong smaller duration")
 }
+
+func TestSummaryWhenForkSingleBlock(t *testing.T) {
+	b := recorder.NewBlocks()
+	now := time.Now()
+	blockNumber := uint64(1000)
+	b.Add(now, blockNumber, "123456")
+	b.Add(now, blockNumber, "654321")
+
+	summary := b.Summary().(recorder.BlocksSummary)
+	assert.True(t, summary.Duration <= time.Second, "wrong duration")
+	assert.Equal(t, 1, len(summary.Forks), "wrong fork count")
+	assert.Equal(t, blockNumber, summary.Forks[0].Begin, "wrong fork start")
+	assert.Equal(t, blockNumber, summary.Forks[0].End, "wrong fork end")
+}
+
+func TestSummaryWhenForkMultipleBlocks(t *testing.T) {
+	b := recorder.NewBlocks()
+	now := time.Now()
+	blockNumber := uint64(1000)
+	b.Add(now, blockNumber, "1")
+	b.Add(now, blockNumber+1, "2")
+	b.Add(now, blockNumber+2, "3")
+	b.Add(now, blockNumber+3, "4")
+	b.Add(now, blockNumber+4, "5")
+	b.Add(now, blockNumber+3, "6")
+	b.Add(now, blockNumber+4, "7")
+	b.Add(now, blockNumber+5, "8")
+
+	summary := b.Summary().(recorder.BlocksSummary)
+	assert.True(t, summary.Duration <= time.Second, "wrong duration")
+	assert.Equal(t, 1, len(summary.Forks), "wrong fork count")
+	assert.Equal(t, blockNumber+3, summary.Forks[0].Begin, "wrong fork start")
+	assert.Equal(t, blockNumber+4, summary.Forks[0].End, "wrong fork end")
+}
+
+func TestSummaryWhenForkMultipleBlocksMultipleTimes(t *testing.T) {
+	b := recorder.NewBlocks()
+	now := time.Now()
+	blockNumber := uint64(1000)
+	b.Add(now, blockNumber, "1")
+	b.Add(now, blockNumber+1, "2")
+	b.Add(now, blockNumber+2, "3")
+	b.Add(now, blockNumber+3, "4")
+	b.Add(now, blockNumber+4, "5")
+	b.Add(now, blockNumber+3, "6")
+	b.Add(now, blockNumber+4, "7")
+	b.Add(now, blockNumber+5, "8")
+	b.Add(now, blockNumber+6, "9")
+	b.Add(now, blockNumber+7, "10")
+	b.Add(now, blockNumber+6, "11")
+	b.Add(now, blockNumber+7, "12")
+
+	summary := b.Summary().(recorder.BlocksSummary)
+	assert.True(t, summary.Duration <= time.Second, "wrong duration")
+	assert.Equal(t, 2, len(summary.Forks), "wrong fork count")
+	assert.Equal(t, blockNumber+3, summary.Forks[0].Begin, "wrong fork start")
+	assert.Equal(t, blockNumber+4, summary.Forks[0].End, "wrong fork start")
+	assert.Equal(t, blockNumber+6, summary.Forks[1].Begin, "wrong fork end")
+	assert.Equal(t, blockNumber+7, summary.Forks[1].End, "wrong fork start")
+}
+
+func TestSummaryWhenForkInProgressAndDropOneBlock(t *testing.T) {
+	b := recorder.NewBlocks()
+	now := time.Now()
+	blockNumber := uint64(1000)
+	b.Add(now, blockNumber, "1")
+	b.Add(now, blockNumber+1, "2")
+	b.Add(now, blockNumber+2, "3")
+	b.Add(now, blockNumber+3, "4")
+	b.Add(now, blockNumber+4, "5")
+	b.Add(now, blockNumber+3, "6")
+	b.Add(now, blockNumber+5, "8")
+
+	summary := b.Summary().(recorder.BlocksSummary)
+	assert.True(t, summary.Duration <= time.Second, "wrong duration")
+	assert.Equal(t, 1, len(summary.Forks), "wrong fork count")
+	assert.Equal(t, blockNumber+3, summary.Forks[0].Begin, "wrong fork start")
+	assert.Equal(t, blockNumber+4, summary.Forks[0].End, "wrong fork start")
+}
+
+func TestSummaryWhenForkBlockRecycledEntirely(t *testing.T) {
+	setupRecorder()
+	ctl, mock := setupTestClock(t)
+	defer ctl.Finish()
+	mock.EXPECT().NewTimer(gomock.Any()).Return(time.NewTimer(1)).Times(1)
+
+	b := recorder.NewBlocks()
+	now := time.Now()
+	threeHourBefore := now.Add(-3 * time.Hour)
+	blockNumber := uint64(1000)
+
+	b.Add(threeHourBefore, blockNumber, "1")
+	b.Add(threeHourBefore, blockNumber+1, "2")
+	b.Add(threeHourBefore, blockNumber+2, "3")
+	b.Add(threeHourBefore, blockNumber+3, "4")
+	b.Add(threeHourBefore, blockNumber+4, "5")
+	b.Add(threeHourBefore, blockNumber+3, "6")
+	b.Add(threeHourBefore, blockNumber+4, "5")
+	b.Add(now, blockNumber+5, "8")
+	b.Add(now, blockNumber+6, "9")
+	b.Add(now, blockNumber+7, "10")
+	b.Add(now, blockNumber+6, "11")
+	b.Add(now, blockNumber+7, "12")
+
+	go b.RemoveOutdatedPeriodically(mock)
+	<-time.After(10 * time.Millisecond)
+	shutdownChan <- struct{}{}
+
+	summary := b.Summary().(recorder.BlocksSummary)
+	assert.True(t, summary.Duration <= time.Second, "wrong duration")
+	assert.Equal(t, 1, len(summary.Forks), "wrong fork count")
+	assert.Equal(t, blockNumber+6, summary.Forks[0].Begin, "wrong fork start")
+	assert.Equal(t, blockNumber+7, summary.Forks[0].End, "wrong fork start")
+}
+
+func TestSummaryWhenForkBlockRecycledPartially(t *testing.T) {
+	setupRecorder()
+	ctl, mock := setupTestClock(t)
+	defer ctl.Finish()
+	mock.EXPECT().NewTimer(gomock.Any()).Return(time.NewTimer(1)).Times(1)
+
+	now := time.Now()
+	b := recorder.NewBlocks()
+	threeHourBefore := now.Add(-3 * time.Hour)
+	blockNumber := uint64(1000)
+
+	b.Add(threeHourBefore, blockNumber, "1")
+	b.Add(threeHourBefore, blockNumber+1, "2")
+	b.Add(threeHourBefore, blockNumber+2, "3")
+	b.Add(threeHourBefore, blockNumber+3, "4")
+	b.Add(now, blockNumber+4, "5")
+	b.Add(now, blockNumber+3, "6")
+	b.Add(now, blockNumber+4, "5")
+	b.Add(now, blockNumber+5, "8")
+
+	go b.RemoveOutdatedPeriodically(mock)
+	<-time.After(10 * time.Millisecond)
+	shutdownChan <- struct{}{}
+
+	summary := b.Summary().(recorder.BlocksSummary)
+	assert.Equal(t, 1, len(summary.Forks), "wrong fork count")
+	assert.Equal(t, blockNumber+3, summary.Forks[0].Begin, "wrong fork start")
+	assert.Equal(t, blockNumber+4, summary.Forks[0].End, "wrong fork start")
+}
