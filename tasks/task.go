bc1e89a7b930982e3a5fdb471b39e0083b920b2e
Aaron Chang <xesuschang@gmail.com>
2019-09-26 09:50:11 +0800

    add tasks

---
+++ tasks/task.go
@@ -0,0 +1,40 @@
+package tasks
+
+import (
+	"sync"
+
+	"golang.org/x/net/context"
+)
+
+type task struct {
+	cancel context.CancelFunc
+	done   chan<- struct{}
+	wg     sync.WaitGroup
+}
+
+// Go - invoke a goroutine to run
+// execution function needs to receive context.Done signal
+func (t *task) Go(exec executionFunction) {
+	go func(t *task) {
+		t.wg.Add(1)
+		exec()
+		t.wg.Done()
+	}(t)
+}
+
+// Done - stop a goroutine
+func (t *task) Done() {
+	t.cancel()
+	t.wg.Wait()
+	t.done <- struct{}{}
+}
+
+// NewTasks - create instance for Tasks interface
+// done is used to notify that all goroutines are terminated
+// cancel is used to send signal to all running goroutines
+func NewTasks(done chan<- struct{}, cancel context.CancelFunc) Tasks {
+	return &task{
+		cancel: cancel,
+		done:   done,
+	}
+}
---
+++ tasks/tasks_test.go
@@ -0,0 +1,47 @@
+package tasks_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/jamieabc/bitmarkd-broadcast-monitor/tasks"
+)
+
+func TestDone(t *testing.T) {
+	done := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	myTask := tasks.NewTasks(done, cancel)
+	var wgGoroutineReady sync.WaitGroup
+	wgGoroutineReady.Add(2)
+
+	myTask.Go(func(args ...interface{}) {
+		wgGoroutineReady.Done()
+		select {
+		case <-ctx.Done():
+			return
+		}
+	})
+
+	myTask.Go(func(args ...interface{}) {
+		wgGoroutineReady.Done()
+		select {
+		case <-ctx.Done():
+			return
+		}
+	})
+	wgGoroutineReady.Wait()
+
+	channelStartWaiting := make(chan struct{})
+	channelReceivedSignal := make(chan struct{})
+
+	go func(ch <-chan struct{}) {
+		channelStartWaiting <- struct{}{}
+		<-ch
+		channelReceivedSignal <- struct{}{}
+	}(done)
+
+	<-channelStartWaiting
+	myTask.Done()
+	<-channelReceivedSignal
+}
